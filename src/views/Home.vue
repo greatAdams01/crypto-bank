7d4bafe532dc122d8763f968fb402b37796e3edc
Ifeanyichukwuwebdeve <kingifean@gmail.com>
2022-02-27 14:58:32 +0100

    Done with home views

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,255 @@
 <template>
-  <div class="container">
-    <h1>Home</h1>
+  <div class="home">
+    <section class="bg-hero">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-md-6 hero-item">
+            <div class="content">
+              <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis eaque in ipsam modi sequi voluptas illo vitae, maxime ex expedita fuga! Perspiciatis similique delectus alias est eaque vero quaerat optio.</p>
+              <button class="btn btn-primary">Get Started</button>
+            </div>
+          </div>
+          <div class="col-12 col-md-6 hero-item">
+            <img src="/img/Money income-rafiki 1.png" alt="">
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="offering text-center">
+      <h2 class="section-title text-center">Our offering</h2>
+      <div class="title-line" />
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-md-4 offering-item">
+            <div class="content">
+              <div class="inner-content">
+                <h4>Stable & Profitable</h4>
+                <p>Experienced business owners and managers know that 3 things are necessary to ensure the long- term success of any business: growth, profit and stability.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-md-4 offering-item">
+            <div class="content">
+              <div class="inner-content">
+                <h4>Instant Withdraw</h4>
+                <p>Withdrawal Speed: Up to 4 hours .General rules for depositing and withdrawing funds . If a deposit or withdrawal is not subject to instant execution.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-md-4 offering-item">
+            <div class="content">
+              <div class="inner-content">
+                <h4>Referral Provides</h4>
+                <p>A referral program is an organized process in which customers are rewarded for spreading the word.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="who-we-are">
+      <div class="container">
+        <h2 class="section-title text-center">Who we are</h2>
+        <div class="title-line" />
+        <div class="content text-center">
+          <h4 class="pb-4">Rexsteelz</h4>
+          <p>RexsteelzInvest remains our overall pick for investors. In addition to refusing payment for order flow, RexsteelzInvest keeps costs low, provides exceptional tools to help investors manage and analyze their portfolios, and continually improves its platforms to make things easier for investors and traders..</p>
+        </div>
+        <div class="row">
+          <div class="col-12 col-md-6 who-items">
+            <div class="content">
+              <h4>Saving & Investments</h4>
+              <p>The Certified Licensing Professional certification is for individuals working in the fields of licensing, business development and commercialization of intellectual.</p>
+            </div>
+          </div>
+          <div class="col-12 col-md-6 who-items">
+            <div class="content">
+              <h4>Saving & Investments</h4>
+              <p>The Certified Licensing Professional certification is for individuals working in the fields of licensing, business development and commercialization of intellectual.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="how-to-invest">
+      <div class="container">
+        <h2 class="section-title text-center">How to invest</h2>
+        <div class="title-line" />
+        <div class="row justify-content-between">
+          <div class="col-12 col-md-5">
+            <div class="text-content">
+              <h5>OPEN AN ACCOUNT</h5>
+              <p>Experienced business owners and managers know that 3 things are necessary to ensure the long- term success of any business: growth, profit and stability.</p>
+            </div>
+            <div class="text-content">
+              <h5>GIVE INTEREST</h5>
+              <p>Withdrawal Speed: Up to 4 hours .General rules for depositing and withdrawing funds . If a deposit or withdrawal is not subject to instant execution.</p>
+            </div>
+            <div class="text-content">
+              <h5>GET DEPOSIT</h5>
+              <p>A referral program is an organized process in which customers are rewarded for spreading the word.</p>
+            </div>
+          </div>
+          <div class="col-12 col-md-6 how-item">
+            <img src="/img/Crypto-portfolio-amico.svg" alt="">
+          </div>
+        </div>
+      </div>
+    </section>
+
+  <section class="investment-package">
+    <div class="container">
+      <h2 class="section-title text-center">Investment Mega Package</h2>
+      <div class="title-line" />
+      <p>Choose the package that best suits your personality today to start your journey to financial freedom</p>
+
+      <div class="row mt-5">
+        <div class="col-12 col-md-4 package-item">
+          <div class="content">
+            <div class="inner-content">
+              <h5 class="pb-2">Basic</h5>
+              <p>Maximum Deposit 4990USD</p>
+              <p>Minimum Deposit 500USD</p>
+              <p>ROI Action 50 TIMES</p>
+              <p>30% Payback Weekly</p>
+
+              <button class="btn btn-primary mt-3">Ghost button</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-4 package-item">
+          <div class="content">
+            <div class="inner-content">
+            <h5 class="pb-2">Standard</h5>
+            <p>Maximum Deposit 4990USD</p>
+            <p>Minimum Deposit 500USD</p>
+            <p>ROI Action 50 TIMES</p>
+            <p>30% Payback Weekly</p>
+
+            <button class="btn btn-primary mt-3">Ghost button</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-4 package-item">
+          <div class="content">
+            <div class="inner-content">
+            <h5 class="pb-2">Silver</h5>
+            <p>Maximum Deposit 4990USD</p>
+            <p>Minimum Deposit 500USD</p>
+            <p>ROI Action 50 TIMES</p>
+            <p>30% Payback Weekly</p>
+
+            <button class="btn btn-primary mt-3">Ghost button</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-4 package-item">
+          <div class="content">
+            <div class="inner-content">
+            <h5 class="pb-2">Golden</h5>
+            <p>Maximum Deposit 4990USD</p>
+            <p>Minimum Deposit 500USD</p>
+            <p>ROI Action 50 TIMES</p>
+            <p>30% Payback Weekly</p>
+
+            <button class="btn btn-primary mt-3">Ghost button</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-4 package-item">
+          <div class="content">
+            <div class="inner-content">
+            <h5 class="pb-2">Diamond</h5>
+            <p>Maximum Deposit 4990USD</p>
+            <p>Minimum Deposit 500USD</p>
+            <p>ROI Action 50 TIMES</p>
+            <p>30% Payback Weekly</p>
+
+            <button class="btn btn-primary mt-3">Ghost button</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-4 package-item">
+          <div class="content">
+            <div class="inner-content">
+            <h5 class="pb-2">Elite</h5>
+            <p>Maximum Deposit 4990USD</p>
+            <p>Minimum Deposit 500USD</p>
+            <p>ROI Action 50 TIMES</p>
+            <p>30% Payback Weekly</p>
+
+            <button class="btn btn-primary mt-3">Ghost button</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="top-investors">
+    <div class="container">
+      <h2 class="section-title text-center">Top investors</h2>
+        <div class="title-line" />
+        <p>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web</p>
+        <div class="row mt-5">
+          <div class="col-12 col-md-3 investor-item">
+            <div class="content">
+              <div class="investor-img" />
+              <h5>Founder</h5>
+              <p>Amanatt</p>
+            </div>
+          </div>
+          <div class="col-12 col-md-3 investor-item">
+            <div class="content">
+              <div class="investor-img" />
+              <h5>Founder</h5>
+              <p>Amanatt</p>
+            </div>
+          </div>
+          <div class="col-12 col-md-3 investor-item">
+            <div class="content">
+              <div class="investor-img" />
+              <h5>Founder</h5>
+              <p>Amanatt</p>
+            </div>
+          </div>
+          <div class="col-12 col-md-3 investor-item">
+            <div class="content">
+              <div class="investor-img" />
+              <h5>Founder</h5>
+              <p>Amanatt</p>
+            </div>
+          </div>
+        </div>
+    </div>
+  </section>
+
+  <section class="latest-news">
+    <div class="container">
+      <h2 class="section-title text-center">Latest News</h2>
+      <div class="title-line" />
+      <div class="row">
+        <div class="col-12 news-item">
+          <div class="news-img">
+            <p>Autrhor // October 10 2020</p>
+          </div>
+          <div class="news-text">
+            <div class="content">
+              <h5>Laoreet feugiat tincidunt lacus sit ut.</h5>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio doloribus porro ipsam autem, asperiores molestiae, minima tenetur distinctio explicabo maxime amet inventore praesentium aliquam. Ea fugiat modi odio sint alias?</p>
+
+              <a href="/">See more</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
   </div>
 </template>
 
